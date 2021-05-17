0997462e7a25b9e8891058915ed2e851ea5100dd
Wxy <awxy115@163.com>
2021-05-18 00:04:05 +0800

    再来一次

--- app/src/main/java/com/wxy/hotfix/ChangeQuickRedirect.java
+++ app/src/main/java/com/wxy/hotfix/ChangeQuickRedirect.java
@@ -1,14 +1,15 @@
-package com.wxy.hotfix
+package com.wxy.hotfix;
 
 /**
  * <pre>
  *     author : wxy
  *     e-mail : awxy115@163.com
- *     time   : 2021/05/17
+ *     time   : 2021/05/18
  *     desc   :
  *     version: 1.0
  * </pre>
  */
 interface ChangeQuickRedirect {
-    fun invoke();
+    void invoke();
+
 }
