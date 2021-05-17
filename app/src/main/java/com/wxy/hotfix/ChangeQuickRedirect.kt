90c9c64d1933f0ccf35d4620b466e5fe5f7d15b1
Wxy <awxy115@163.com>
2021-05-17 17:03:28 +0800

    commit ChangeQuickRedirect

---
+++ app/src/main/java/com/wxy/hotfix/ChangeQuickRedirect.kt
@@ -0,0 +1,14 @@
+package com.wxy.hotfix
+
+/**
+ * <pre>
+ *     author : wxy
+ *     e-mail : awxy115@163.com
+ *     time   : 2021/05/17
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+interface ChangeQuickRedirect {
+    fun invoke();
+}
