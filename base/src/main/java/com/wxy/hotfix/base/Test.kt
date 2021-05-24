722e373efc02e0451cf85a172a7eab3146547c6f
Wxy <awxy115@163.com>
2021-05-24 15:43:50 +0800

    再来一次

---
+++ base/src/main/java/com/wxy/hotfix/base/Test.kt
@@ -0,0 +1,17 @@
+package com.wxy.hotfix.base
+
+
+/**
+ * <pre>
+ *     author : wxy
+ *     e-mail : awxy115@163.com
+ *     time   : 2021/05/24
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+class Test {
+    public fun toast() {
+        val a = 0 + 1;
+    }
+}
